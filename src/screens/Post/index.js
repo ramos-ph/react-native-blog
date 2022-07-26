a8ff18126b8464a575a35c8a551b7f0ae41da1ba
Pedro Ramos <pedroramos.contato@outlook.com>
2022-07-26 08:51:08 -0300

    Update Post component to render post by id
    Instead of a hardcoded markdown content, fetch it from an API

--- src/screens/Post/index.js
+++ src/screens/Post/index.js
@@ -1,44 +1,52 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {useRoute} from '@react-navigation/native'
 
 import {Container, PostTitle, PostDate, Paragraph, ListItem} from './styles'
 import BackToHomeButton from './components/BackToHomeButton'
 import AuthorBrief from './components/AuthorBrief'
 
-const postContent = `# Something about tests
-
-This is a blog post that mentions something about tests.
-
-- Tests must be clean
-- Tests must run fast
-- Tests must be isolated
-
-Please drop a like`
+import api from '../../services/api'
+import {format, parseISO} from 'date-fns'
 
 function renderPostContent(markdown) {
+	if (!markdown) return
+
 	return markdown
 		.split('\n')
 		.filter(line => line)
-		.map(line => {
-			if (line.startsWith('#')) {
-				return (
-					<>
-						<PostTitle>{line.replace('#', '').trim()}</PostTitle>
-						<PostDate>February 13, 2022</PostDate>
-					</>
-				)
-			} else if (line.startsWith('-')) {
-				return <ListItem>{line}</ListItem>
+		.map((line, index) => {
+			if (line.startsWith('-')) {
+				return <ListItem key={index}>{line}</ListItem>
 			} else {
-				return <Paragraph>{line}</Paragraph>
+				return <Paragraph key={index}>{line}</Paragraph>
 			}
 		})
 }
 
-function Post({postId}) {
+function Post() {
+	const [post, setPost] = useState({})
+
+	const route = useRoute()
+	const postId = route.params.postId
+
+	useEffect(() => {
+		async function fetchPostContent() {
+			const response = await api.get(`/posts/${postId}`)
+			setPost(response.data)
+		}
+
+		fetchPostContent()
+	}, [postId])
+
 	return (
 		<Container>
-			<AuthorBrief />
-			{renderPostContent(postContent)}
+			<AuthorBrief name={post.author} />
+			<PostTitle>{post.title?.replace('#', '').trim()}</PostTitle>
+			<PostDate>
+				{post.created_at && format(parseISO(post.created_at), 'LLLL d, yyyy')}
+			</PostDate>
+
+			{renderPostContent(post?.content)}
 			<BackToHomeButton />
 		</Container>
 	)
